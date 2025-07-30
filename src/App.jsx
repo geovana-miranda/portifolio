f5174e8eec5115e7c7a7385ee1e56fbb94fb1144
Geovana Miranda <182geovana@gmail.com>
2025-07-30 16:34:39 -0300

    feat: adicionar seção Hero com layout responsivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-    return (
-    <>
-      
-        
-    </>
-  )
+  return (
+    <main className="border-2 mx-4 my-2">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,27 @@
+import profile from "../assets/profile.jpeg";
+
+const HeroSection = () => {
+  return (
+    <section className="flex flex-col items-center gap-6 py-10 md:flex-row md:justify-center md:gap-16">
+      <div className="">
+        <div className="relative">
+          <div className="w-52 absolute top-1 inset-y-0 inset-x-4 bg-amber-400 rounded-full border-3 z-0"></div>
+          <img
+            src={profile}
+            alt=""
+            className="relative z-10 border w-52 rounded-full"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-4 font-semibold">
+        <h2 className="text-xl md:text-2xl">Olá! Eu sou Geovana Miranda</h2>
+        <h1 className="text-2xl md:text-4xl">Desenvolvedora Front-End</h1>
+        <button className="mt-4 bg-purple-300 text-black font-semibold py-2 px-8 rounded drop-shadow-[4px_4px_0_rgba(0,0,0,0.7)] md:px-16">
+          Baixar CV
+        </button>          
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
