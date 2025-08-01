c2fb0e99173495aba0725a1a98e042cd0766be0c
Geovana Miranda <182geovana@gmail.com>
2025-08-01 15:00:09 -0300

    style: refatora estilos

--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ name, image, description, color, colorButton, github, vercel }) => {
   return (
-    <div className="w-68 h-[445px] rounded-2xl border-2 border-black shadow-[6px_6px_0px_black] bg-white mb-6 flex flex-col">
+    <div className="w-80 h-auto md:w-md md:h-[550px] rounded-2xl border-2 border-black shadow-[6px_6px_0px_black] bg-white mb-6 flex flex-col">
       <div className={`${color} rounded-t-2xl p-4 border-b-2`}>
         <h3 className="text-center text-xl font-bold border-black p-2">
           {name}
@@ -9,11 +9,11 @@ const Card = ({ name, image, description, color, colorButton, github, vercel }) 
       <img
         src={image}
         alt="preview do projeto"
-        className="h-[180px] w-full object-contain "
+        className="h-[200px] md:h-[280px] w-full object-contain "
       />
-      <div className="p-4 flex flex-col flex-1">
-        <p className="text-sm mb-2">{description}</p>
-        <div className="flex justify-between mt-auto">
+      <div className="p-4 md:px-8 md:py-6 flex flex-col flex-1">
+        <p className="text-sm mb-6">{description}</p>
+        <div className="flex justify-end gap-6 mt-auto">
           <a
             href={github}
             target="_blank"
