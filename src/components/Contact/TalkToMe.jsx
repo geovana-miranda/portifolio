665952db05583419246daf69f2bbd8b5b8edaa7c
Geovana Miranda <182geovana@gmail.com>
2025-08-02 12:56:36 -0300

    refactor: reorganiza componente Contact e atualiza informações de contato

--- src/components/Contact/TalkToMe.jsx
+++ src/components/Contact/TalkToMe.jsx
@@ -1,9 +1,44 @@
+import SocialButton from "./SocialButton";
+
 const TalkToMe = () => {
   return (
-    <div className="flex flex-col items-center gap-4">
-      <h4 className="font-bold text-xl text-green-500">Vamos trabalhar juntos!</h4>
-      <p className="text-sm md:text-base text-center font-semibold">Se você curtiu meu portfólio, tem um projeto legal ou só quer bater um papo, é só me chamar.
-      </p>
+    <div className="w-80 mx-auto md:w-lg h-[380px] border-2 rounded-2xl flex flex-col justify-around items-center p-4 bg-white shadow-[5px_5px_0px_black] md:flex md:flex-col">
+      <div className="flex flex-col items-center gap-4">
+        <h4 className="font-bold text-xl text-green-500">
+          Vamos trabalhar juntos?
+        </h4>
+        <div className="flex flex-col items-start">
+          <div className="flex items-center gap-2">
+            <div className="text-green-500 text-xl">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+            </div>
+            <p className="text-base text-center font-semibold">
+              182geovana@gmail.com
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="text-green-500 text-xl">
+              <i class="fa fa-phone-square" aria-hidden="true"></i>{" "}
+            </div>
+            <p className="text-base text-center font-semibold">
+              (11) 9-4186-3282
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mx-auto items-center flex flex-col justify-center gap-4 flex-wrap lg:w-full">
+            <h4 className="text-xl font-bold text-green-500 ">Minhas redes:</h4>
+            <SocialButton
+              icon="devicon-github-original"
+              name="Github"
+              link="https://github.com/geovana-miranda"
+            />
+            <SocialButton
+              icon="devicon-linkedin-plain"
+              name="Linkedin"
+              link="https://www.linkedin.com/in/geovana-miranda/"
+            />
+          </div>
     </div>
   );
 };
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,16 +1,17 @@
 import { useInView } from "../../hooks/useInView";
 import Form from "./Form";
-import SocialButton from "./SocialButton";
 import TalkToMe from "./TalkToMe";
 
 const Contact = () => {
-const [ref, isVisible] = useInView();
+  const [ref, isVisible] = useInView();
 
   return (
     <section ref={ref} className="py-16 mx-auto bg-blue-600">
-      <h2 className={`transition-all duration-[1s] ease ${
+      <h2
+        className={`transition-all duration-[1s] ease ${
           isVisible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-30"
-        } text-4xl text-center mb-12 font-bold text-green-500`}>
+        } text-4xl text-center mb-12 font-bold text-green-500`}
+      >
         Contato
       </h2>
 
@@ -20,24 +21,7 @@ const [ref, isVisible] = useInView();
         } flex flex-col md:flex-row md:items-center lg:justify-center w-sm md:w-3xl lg:w-4xl mx-auto gap-12`}
       >
         <Form />
-
-        <div className="w-80 mx-auto md:w-lg h-[380px] border-2 rounded-2xl flex flex-col justify-around items-center p-8 bg-white shadow-[5px_5px_0px_black] md:flex md:flex-col md:gap-4">
-          <TalkToMe />
-
-          <div className="mx-auto items-center flex flex-col justify-center gap-4 flex-wrap lg:w-full">
-            <h4 className="text-xl font-bold text-green-500 ">Minhas redes:</h4>
-            <SocialButton
-              icon="devicon-github-original"
-              name="Github"
-              link="https://github.com/geovana-miranda"
-            />
-            <SocialButton
-              icon="devicon-linkedin-plain"
-              name="Linkedin"
-              link="https://www.linkedin.com/in/geovana-miranda/"
-            />
-          </div>
-        </div>
+        <TalkToMe />
       </div>
     </section>
   );
